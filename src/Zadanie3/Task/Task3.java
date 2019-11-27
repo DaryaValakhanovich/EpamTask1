50c7fbcab36f50740da2f8af9a9c9b06ed7b955a
Даша <valakhanovichdasha@mail.ru>
2019-11-27 23:33:35 +0300

    Commit1

--- src/Zadanie3/Task/Task3.java
+++ src/Zadanie3/Task/Task3.java
@@ -13,8 +13,8 @@ public class Task3 {
     //максимум – это элемент, который больше любого из своих соседей);
     public static void main(String[] args) {
         double[][] array = new double[][]{
-                {1.2, 4.6, 7.8},
-                {8.2, 0.9, 7.4},
+                {1.2, 1.0, 7.8},
+                {3.1, 0.9, 7.4},
                 {0.2, 3.6, 7.8},
                 {1.2, 8.6, 7.8}
         };
@@ -32,91 +32,69 @@ public class Task3 {
     private static int[] localMin(double[][] array){
         int[] resultArray;
         int n=array.length-1,m =array[0].length-1;
-        resultArray=localMinAngles(array);
-        if(resultArray[0]!=-1){
-            return resultArray;
-        }
-        resultArray=localMinFirstLastLine(array);
-        if(resultArray[0]!=-1){
-            return resultArray;
-        }
-        resultArray=localMinFirstLastColumn(array);
+        resultArray=localMinFirstLine(array);
         if(resultArray[0]!=-1){
             return resultArray;
         }
         for (int i = 1; i < n; i++) {
+            if (array[i][0] < array[i-1][0] && array[i][0] < array[i][1] && array[i][0] < array[i+1][0]){
+                resultArray[0]=i;
+                resultArray[1]=0;
+                return resultArray;
+            }
             for (int j = 1; j < m; j++) {
-                if (array[i][j] < array[i - 1][j - 1] && array[i][j] < array[i - 1][j] && array[i][j] < array[i - 1][j + 1]
+                if (array[i][j] < array[i - 1][j]
                         && array[i][j] < array[i][j - 1] && array[i][j] < array[i][j + 1]
-                        && array[i][j] < array[i + 1][j - 1] && array[i][j] < array[i + 1][j] && array[i][j] < array[i + 1][j + 1]) {
+                        && array[i][j] < array[i + 1][j]) {
                     resultArray[0] = i;
                     resultArray[1] = j;
                     return resultArray;
                 }
             }
-        }
-        return resultArray;
-    }
-
-    private static int[] localMinAngles(double[][] array) {
-        int[] resultArray = new int[]{-1,-1};
-        int n=array.length-1, m=array[0].length-1;
-        if (array[0][0] < array[0][1]
-                && array[0][0] < array[1][0] && array[0][0] < array[1][1]) {
-            resultArray[0] = 0;
-            resultArray[1] = 0;
-            return resultArray;
-        }
-        if (array[0][m] < array[0][m - 1]
-                && array[0][m] < array[1][m - 1] && array[0][m] < array[1][m]) {
-            resultArray[0] = 0;
-            resultArray[1] = m;
-            return resultArray;
-        }
-        if (array[n][0] < array[n -1][0] && array[n][0] < array[n - 1][1]
-               && array[n][0] < array[n][1]) {
-            resultArray[0] = n;
-            resultArray[1] = 0;
-            return resultArray;
-        }
-        if (array[n][m] < array[n - 1][m - 1] && array[n][m] < array[n - 1][m]
-                && array[n][m] < array[n][m - 1]) {
-            resultArray[0] = n;
-            resultArray[1] = m;
-            return resultArray;
-        }
-        return resultArray;
-    }
-    private static int[] localMinFirstLastColumn(double[][] array) {
-        int[] resultArray = new int[]{-1,-1};
-        int n=array.length-1, m=array[0].length-1;
-        for (int i = 1; i < n; i++) {
-            if (array[i][0] < array[i-1][0] && array[i][0] < array[i-1][1]
-                    && array[i][0] < array[i][1]
-                    && array[i][0] < array[i+1][0] && array[i][0] < array[i+1][1]){
-                resultArray[0]=i;
-                resultArray[1]=0;
-                return resultArray;
-            }
-            if (array[i][m] < array[i-1][m-1] && array[i][m] < array[i-1][m]
-                    && array[i][m] < array[i][m]
-                    && array[i][m] < array[i+1][m-1] && array[i][m] < array[i+1][m]){
+            if (array[i][m] < array[i-1][m] && array[i][m] < array[i][m] && array[i][m] < array[i+1][m]){
                 resultArray[0]=i;
                 resultArray[1]=m;
                 return resultArray;
             }
         }
+        resultArray=localMinLastLine(array);
+        if(resultArray[0]!=-1){
+            return resultArray;
+        }
         return resultArray;
     }
-    private static int[] localMinFirstLastLine(double[][] array) {
+
+    private static int[] localMinFirstLine(double[][] array) {
         int[] resultArray = new int[]{-1,-1};
         int n=array.length-1, m=array[0].length-1;
+        if (array[0][0] < array[1][0] && array[0][0] < array[0][1]) {
+            resultArray[0] = 0;
+            resultArray[1] = 0;
+            return resultArray;
+        }
         for (int j = 1; j < m; j++) {
             if (array[0][j] < array[1][j - 1] && array[0][j] < array[1][j] && array[0][j] < array[1][j + 1]) {
                 resultArray[0] = 0;
                 resultArray[1] = j;
                 return resultArray;
             }
+        }
+        if (array[0][m] < array[0][m - 1] && array[0][m] < array[1][m]) {
+            resultArray[0] = 0;
+            resultArray[1] = m;
+            return resultArray;
+        }
+        return resultArray;
+    }
+    private static int[] localMinLastLine(double[][] array) {
+        int[] resultArray = new int[]{-1,-1};
+        int n=array.length-1, m=array[0].length-1;
+        if (array[0][0] < array[1][0] && array[0][0] < array[0][1]) {
+            resultArray[0] = 0;
+            resultArray[1] = 0;
+            return resultArray;
+        }
+        for (int j = 1; j < m; j++) {
             if (array[n][j] < array[n - 1][j - 1] && array[n][j] < array[n - 1][j] && array[n][j] < array[n - 1][j + 1]
                     && array[n][j] < array[n][j - 1] && array[n][j] < array[n][j + 1]) {
                 resultArray[0] = n;
@@ -124,103 +102,91 @@ public class Task3 {
                 return resultArray;
             }
         }
+        if (array[0][m] < array[0][m - 1] && array[0][m] < array[1][m]) {
+            resultArray[0] = 0;
+            resultArray[1] = m;
+            return resultArray;
+        }
         return resultArray;
     }
 
     private static int[] localMax(double[][] array){
         int[] resultArray;
         int n=array.length-1,m =array[0].length-1;
-        resultArray=localMaxAngles(array);
-        if(resultArray[0]!=-1){
-            return resultArray;
-        }
-        resultArray=localMaxFirstLastLine(array);
-        if(resultArray[0]!=-1){
-            return resultArray;
-        }
-        resultArray=localMaxFirstLastColumn(array);
+        resultArray=localMaxFirstLine(array);
         if(resultArray[0]!=-1){
             return resultArray;
         }
         for (int i = 1; i < n; i++) {
+            if (array[i][0] > array[i-1][0] && array[i][0] > array[i][1] && array[i][0] > array[i+1][0]){
+                resultArray[0]=i;
+                resultArray[1]=0;
+                return resultArray;
+            }
             for (int j = 1; j < m; j++) {
-                if (array[i][j] > array[i - 1][j - 1] && array[i][j] > array[i - 1][j] && array[i][j] > array[i - 1][j + 1]
+                if (array[i][j] > array[i - 1][j]
                         && array[i][j] > array[i][j - 1] && array[i][j] > array[i][j + 1]
-                        && array[i][j] > array[i + 1][j - 1] && array[i][j] > array[i + 1][j] && array[i][j] > array[i + 1][j + 1]) {
+                        && array[i][j] > array[i + 1][j]) {
                     resultArray[0] = i;
                     resultArray[1] = j;
                     return resultArray;
                 }
             }
-        }
-        return resultArray;
-    }
-
-    private static int[] localMaxAngles(double[][] array) {
-        int[] resultArray = new int[]{-1,-1};
-        int n=array.length-1, m=array[0].length-1;
-        if (array[0][0] > array[0][1]
-                && array[0][0] > array[1][0] && array[0][0] > array[1][1]) {
-            resultArray[0] = 0;
-            resultArray[1] = 0;
-            return resultArray;
-        }
-        if (array[0][m] > array[0][m - 1]
-                && array[0][m] > array[1][m - 1] && array[0][m] > array[1][m]) {
-            resultArray[0] = 0;
-            resultArray[1] = m;
-            return resultArray;
-        }
-        if (array[n][0] > array[n -1][0] && array[n][0] > array[n - 1][1]
-                && array[n][0] < array[n][1]) {
-            resultArray[0] = n;
-            resultArray[1] = 0;
-            return resultArray;
-        }
-        if (array[n][m] > array[n - 1][m - 1] && array[n][m] > array[n - 1][m]
-                && array[n][m] < array[n][m - 1]) {
-            resultArray[0] = n;
-            resultArray[1] = m;
-            return resultArray;
-        }
-        return resultArray;
-    }
-    private static int[] localMaxFirstLastColumn(double[][] array) {
-        int[] resultArray = new int[]{-1,-1};
-        int n=array.length-1, m=array[0].length-1;
-        for (int i = 1; i < n; i++) {
-            if (array[i][0] > array[i-1][0] && array[i][0] > array[i-1][1]
-                    && array[i][0] > array[i][1]
-                    && array[i][0] > array[i+1][0] && array[i][0] > array[i+1][1]){
-                resultArray[0]=i;
-                resultArray[1]=0;
-                return resultArray;
-            }
-            if (array[i][m] > array[i-1][m-1] && array[i][m] > array[i-1][m]
-                    && array[i][m] > array[i][m]
-                    && array[i][m] > array[i+1][m-1] && array[i][m] > array[i+1][m]){
+            if (array[i][m] > array[i-1][m] && array[i][m] > array[i][m] && array[i][m] > array[i+1][m]){
                 resultArray[0]=i;
                 resultArray[1]=m;
                 return resultArray;
             }
         }
+        resultArray=localMaxLastLine(array);
+        if(resultArray[0]!=-1){
+            return resultArray;
+        }
         return resultArray;
     }
-    private static int[] localMaxFirstLastLine(double[][] array) {
+
+    private static int[] localMaxFirstLine(double[][] array) {
         int[] resultArray = new int[]{-1,-1};
         int n=array.length-1, m=array[0].length-1;
+        if (array[0][0] > array[1][0] && array[0][0] > array[0][1]) {
+            resultArray[0] = 0;
+            resultArray[1] = 0;
+            return resultArray;
+        }
         for (int j = 1; j < m; j++) {
             if (array[0][j] > array[1][j - 1] && array[0][j] > array[1][j] && array[0][j] > array[1][j + 1]) {
                 resultArray[0] = 0;
                 resultArray[1] = j;
                 return resultArray;
             }
+        }
+        if (array[0][m] > array[0][m - 1] && array[0][m] > array[1][m]) {
+            resultArray[0] = 0;
+            resultArray[1] = m;
+            return resultArray;
+        }
+        return resultArray;
+    }
+    private static int[] localMaxLastLine(double[][] array) {
+        int[] resultArray = new int[]{-1, -1};
+        int n = array.length - 1, m = array[0].length - 1;
+        if (array[0][0] < array[1][0] && array[0][0] < array[0][1]) {
+            resultArray[0] = 0;
+            resultArray[1] = 0;
+            return resultArray;
+        }
+        for (int j = 1; j < m; j++) {
             if (array[n][j] > array[n - 1][j - 1] && array[n][j] > array[n - 1][j] && array[n][j] > array[n - 1][j + 1]
-                    && array[n][j] < array[n][j - 1] && array[n][j] < array[n][j + 1]) {
+                    && array[n][j] > array[n][j - 1] && array[n][j] > array[n][j + 1]) {
                 resultArray[0] = n;
                 resultArray[1] = j;
                 return resultArray;
             }
+        }
+        if (array[0][m] < array[0][m - 1] && array[0][m] < array[1][m]) {
+            resultArray[0] = 0;
+            resultArray[1] = m;
+            return resultArray;
         }
         return resultArray;
     }
